af7a0aff07c8b7dd145ff125ddcf150749399907
Simen A. W. Olsen <so@bjerk.io>
2022-12-31 01:29:02 +0100

    add sveltekit boilerplate

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,23 @@
+<script>
+  import "@fontsource/outfit";
+  import "@fontsource/outfit/600.css";
+</script>
+
+<style>
+  :global(body) {
+    --primary-color: #344e4c;
+
+    font-family: "Outfit", sans-serif;
+    color: var(--primary-color);
+    
+    font-smooth: always;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+</style>
+
+<main>
+  <slot>
+    <!-- +page.svelte is rendered here -->
+  </slot>
+</main>
