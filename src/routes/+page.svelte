8db1c6ced4f7a1eef18e9e5b9c13cea83cf57169
Simen A. W. Olsen <so@bjerk.io>
2022-12-31 01:34:42 +0100

    add content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,14 @@
 <div>
 	<h1>Klynge Næringsforening</h1>
 	<p>
-		Klynge Næringsforening er en forening skapt for å hjelpe hverandre å lykkes. Det gjør vi gjennom å fremsnakke, dele erfaringer og samarbeide. En forening skapt for mennesker med interesse for forretningsliv.
+		Klynge Næringsforening er en forening skapt for å hjelpe hverandre å lykkes. Det gjør vi gjennom
+		å fremsnakke, dele erfaringer og samarbeide. En forening skapt for mennesker med interesse for
+		forretningsliv.
 	</p>
-  <p>
-    <b>Vi jobber med å lage en nettside.</b> Enn så lenge, kan du ta kontakt med Bjørn Niklas på b@obj.no for å vite mer!
-  </p>
+	<p>
+		<b>Vi jobber med å lage en nettside.</b> Enn så lenge, kan du ta kontakt med Bjørn Niklas på b@obj.no
+		for å vite mer!
+	</p>
 </div>
 
 <style>
