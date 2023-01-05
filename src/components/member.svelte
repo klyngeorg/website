02632e6d9299f1c1d43c812148ab22858e49aa4f
Simen A. W. Olsen <so@bjerk.io>
2023-01-05 13:02:59 +0100

    feat: add members

---
+++ src/components/member.svelte
@@ -0,0 +1,42 @@
+<article>
+	<img src="https://via.placeholder.com/150" alt="Bilde av Aleksander Ryan" />
+	<section>
+		<div>
+			<header>Bjørn Niklas Sjøstrøm</header>
+			<p>Partner ved Wahl-Larsen Advokatfirma</p>
+			<p>Styremedlem i Klynge</p>
+		</div>
+		<div>
+			<p>+47 40 90 50 40</p>
+			<p>bobbafett@fett.no</p>
+		</div>
+	</section>
+</article>
+
+<style>
+	article {
+		display: flex;
+		gap: var(--space-s);
+
+		font-size: var(--font-size-base);
+	}
+
+	article p {
+    margin: 0;
+	}
+
+	img {
+		height: 100%;
+	}
+
+	section {
+		display: flex;
+		flex-direction: column;
+		gap: var(--space-xs);
+	}
+
+	header {
+		font-size: var(--font-size-md);
+		font-weight: bold;
+	}
+</style>
---
+++ src/routes/about/members/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+	import Member from '../../../components/member.svelte';
+</script>
+
+<grid>
+	<Member />
+  <Member />
+  <Member />
+  <Member />
+  <Member />
+  <Member />
+</grid>
+
+<style>
+  grid {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(400px, 1fr));
+    grid-gap: var(--space-xl);
+    margin-top: var(--space-2xl);
+  }
+</style>
