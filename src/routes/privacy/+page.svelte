249f7fb6cec25b052c54ed78a7719694b4b9990d
Simen A. W. Olsen <so@bjerk.io>
2023-02-23 10:15:46 +0100

    feat: add privacy terms

---
+++ src/routes/privacy/+page.svelte
@@ -0,0 +1,114 @@
+<div class="content">
+	<h1>Personvern­erklæring</h1>
+
+	<p>Her kan du lese om hvordan Klynge Næringsforening samler inn og bruker personopplysninger.</p>
+
+	<h2>Dine rettigheter</h2>
+	<p>
+		Alle har rett på grunnleggende informasjon om behandlinger av personopplysninger i en virksomhet
+		etter personopplysningslovens § 18, 1. ledd. Klynge Næringsforening utgir informasjon om dette i
+		denne erklæringen. Alle som er registrert i våre systemer har rett på innsyn i egne
+		opplysninger. Samt rett til å be om at uriktige, ufullstendige eller opplysninger foreningen
+		ikke har adgang til å behandle blir rettet, slettet eller supplert.
+	</p>
+	<p>Krav fra den registrerte skal besvares kostnadsfritt og senest innen 30 dager.</p>
+
+	<h2>Medlemsregisteret</h2>
+	<p>
+		Alle medlemmer av foreningen må registrere personopplysninger, herunder fornavn, etternavn,
+		e-post, telefonnummer, adresse, arbeidssted og -stilling. Dersom medlemmer ønsker det, legges
+		fullt navn, e-post, telefonnummer, eventuelt bilde og opplysninger om arbeidssted og -stilling
+		på Klynge Næringsforenings nettside.
+	</p>
+
+	<p>
+		Opplysningene lagres hos Sanity AS (og deres underleverandører) og av den grunn vår
+		databehandler. Les selskapets personvvernerklæring her:
+		<a href="https://www.sanity.io/legal/privacy">https://www.sanity.io/legal/privacy</a>
+	</p>
+
+	<p>
+		Kildekode over vårt medlemsregister utgis under åpen kildekode, og kan ses her:
+		<a href="https://github.com/klyngeorg/studio">https://github.com/klyngeorg/studio</a>
+	</p>
+
+	<h2>Påmelding til våre arrangementer</h2>
+
+	<p>
+		Når du melder deg på et av våre arrangementer vil opplysninger som fornavn, etternavn, e-post,
+		telefonnummer, arbeidssted og andre nødvendige opplysninger lagres hos vår databehandler Google.
+		Opplysningene lagres kun midlertidig og kan deles med våre samarbeidspartnere, slik som
+		restauranter, arrangører og utleiere. Alle opplysninger som deles videre anonymiseres om det er
+		mulig.
+	</p>
+
+	<p>
+		I forkant og etterkant av arrangementet kan opplysningene også brukes til å kommunisere til de
+		påmeldte via e-post. Vi sletter opplysningene etter senest 60 dager etter arrangementet er
+		gjennomført eller avlyst.
+	</p>
+
+	<h2>Folk i Klynge</h2>
+
+	<p>
+		Alle som er interessert i Klynge, som ønsker å holde seg oppdatert og engasjert i foreningen,
+		både medlemmer og tidligere gjester kan registrere seg i <em>Folk i Klynge</em>. Vi sender
+		e-post ut til registrerte i Folk i Klynge ca. 2-3 ganger hver måned.
+	</p>
+
+	<p>
+		Avregistrering gjøres ved å sende e-post til
+		<a href="mailto:folk+unsubscribe@klyngeorg.no">folk+unsubscribe@klyngeorg.no</a>
+		eller ved henvendelse hos personvernombudet. Ved registrering vil e-postadresse lagres hos vår databehandler
+		Google.
+	</p>
+
+	<h2>Github og Google</h2>
+
+	<p>
+		I forbindelse med drift av foreningens e-post og kontorverktøy benyttes Google Workspace.
+		Styrets medlemmer og andre engasjert i foreningen vil ha adgang til egne kontoer (herunder blant
+		annet e-post).
+	</p>
+
+	<p>
+		E-post sammen med kontaktopplysninger på mottatte og sendte e-poster kan lagret opptil tre år.
+		E-post vil automatisk slettes ved avslutting av kontoer eller etter tre år. Personopplysninger
+		lagres kun i kontrollerte filområder, hvor nedlasting og annen distribusjon er sperret.
+	</p>
+
+	<h2>Slack</h2>
+
+	<p>
+		Klynge Næringsforening benytter Slack for å skape et sted hvor medlemmer, gjester og andre
+		interessert i foreningen å møtes digitalt. Når man registrerer seg på Slack hentes det inn
+		opplysninger om navn, tidssone, bilde og arbeidssted og -tittel.
+	</p>
+
+	<p>
+		Se også Slacks egen personvernerklæring:
+		<a href="https://slack.com/trust/privacy/privacy-policy">
+			https://slack.com/trust/privacy/privacy-policy
+		</a>
+	</p>
+
+	<h2>Kontakt oss</h2>
+
+	<p>
+		<strong>E-mail:</strong> hei@klyngeorg.no.
+	</p>
+	<p>
+		<strong>Postadresse:</strong><br />
+		Wahl-Larsen Advokatfirma AS<br />
+		℅ Aleksander Ryan<br />
+		Fridtjof Nansens plass 5<br />
+		0160 OSLO
+	</p>
+</div>
+
+<style>
+	.content {
+		max-width: 60ch;
+		margin: 0 auto;
+	}
+</style>
