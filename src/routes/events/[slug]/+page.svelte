ed16049ca8ef4d443fba147f601b8c4c8b225d3f
Simen A. W. Olsen <so@bjerk.io>
2023-05-09 15:30:05 +0200

    wip

--- src/routes/events/[slug]/+page.svelte
+++ src/routes/events/[slug]/+page.svelte
@@ -1,12 +1,239 @@
 <script lang="ts">
-	import type { PageData } from './$types';
-	import Event from '../event.svelte';
-	export let data: PageData;
-	const { event } = data;
+	import { notEmpty, serializeSchema } from '../../../utils';
+	import type { WithContext, Event, Offer, ItemAvailability } from 'schema-dts';
+	import { invariant } from 'ts-invariant';
+	import { defaultEventData } from '../../../libs/events/default-event-data';
+	import type { EventData } from '../../../libs/events/types';
+	export let data: EventData;
+  
+	const event: WithContext<Event> = {
+		'@context': 'https://schema.org',
+		...defaultEventData,
+		name: data.name,
+		startDate: data.startDate.toISOString(),
+		endDate: data.endDate.toISOString(),
+		location: {
+			'@type': 'Place',
+			address: data.location ?? 'Oslo, Norway'
+		},
+		image: new URL(
+			['assets', 'images', 'social', `${data.slug}.png`].join('/'),
+			'https://klyngeorg.no/'
+		).toString(),
+		description: data.description
+	};
+
+	if (data.offers) {
+		event.offers = data.offers
+			.filter((offerItem) => offerItem && offerItem.url)
+			.map((offerItem) => {
+				invariant(offerItem && offerItem.url, 'Offer must have a url');
+				const offer: Offer = {
+					'@type': 'Offer',
+					url: offerItem.url
+				};
+
+				if (offerItem.price) {
+					offer.price = offerItem.price;
+				}
+
+				if (offerItem.priceCurrency) {
+					offer.priceCurrency = offerItem.priceCurrency;
+				}
+
+				if (offerItem.availability) {
+					// TODO: Parse to ItemAvailability
+					offer.availability = offerItem.availability as ItemAvailability;
+				}
+
+				return offer;
+			});
+	}
+
+	const date = new Date(data.startDate).toLocaleDateString('no-NO', {
+		weekday: 'long',
+		month: 'long',
+		day: 'numeric',
+		year: 'numeric'
+	});
+
+	const time = new Date(data.startDate).toLocaleTimeString('no-NO', {
+		hour: 'numeric',
+		minute: 'numeric'
+	});
+
+	const timeFinished = new Date(data.endDate).toLocaleTimeString('no-NO', {
+		hour: 'numeric',
+		minute: 'numeric'
+	});
+
+	const offers = (data.offers ?? []).filter(notEmpty);
+	console.log({ offers });
 </script>
 
-{#if event}
-	<Event data={event} />
-{:else}
-	<p>Fant ikke dette arrangementet.</p>
-{/if}
+<svelte:head>
+	{@html serializeSchema(event)}
+</svelte:head>
+
+<article>
+	<aside class="date">
+		{date}
+		<div class="time">
+			{time}
+		</div>
+	</aside>
+	<main>
+		<a href={`/events/${data.slug}`} rel="noreferrer" target="_blank" class="heading">
+			<h2>
+				{data.name}
+			</h2>
+		</a>
+		{#if data.location}
+			<p class="location">
+				Sted:
+				<a
+					href={'https://www.google.com/maps/search/?api=1&query=' + data.location}
+					rel="noreferrer"
+					target="_blank"
+				>
+					{data.location}
+				</a>
+			</p>
+		{/if}
+		<p>{@html data.description}</p>
+		<p>
+			<small>Anslått ferdig: {timeFinished}</small>
+		</p>
+		{#each offers as offer}
+			<div class="offer-box">
+				<div class="description">
+					{#if offer.price}
+						<p><strong>{offer.price} {offer.priceCurrency}</strong></p>
+					{/if}
+					{#if offer.description}
+						<p class="description-paragraph">
+							{@html offer.description.split('\n').join('<br />')}
+						</p>
+					{/if}
+					{#if offer.availability === 'LimitedAvailability'}
+						<p><strong>Merk:</strong> Det er begrensede plasser</p>
+					{/if}
+					{#if offer.availability === 'SoldOut'}
+						<p><strong>Varsel:</strong> Det er ikke flere plasser</p>
+					{/if}
+					{#if offer.availability === 'BackOrder'}
+						<p><strong>Varsel:</strong> Det er venteliste</p>
+					{/if}
+				</div>
+				<div class="actions">
+					<a href={offer.url} class="button" rel="noreferrer" target="_blank"> Påmelding → </a>
+				</div>
+			</div>
+		{/each}
+	</main>
+</article>
+
+<style>
+	article {
+		display: grid;
+		grid-template-columns: 1fr 3fr;
+		gap: var(--space-s);
+		background-color: var(--light-bg-color);
+		box-shadow: 0px 0px 0px 0px var(--fade-y-color);
+		transition: box-shadow 0.1s ease-in-out;
+		border-radius: 16px;
+	}
+
+	.heading {
+		text-decoration: none;
+	}
+
+	.date {
+		border-radius: 16px 0 0 16px;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+
+		padding: var(--space-s);
+
+		background-color: var(--primary-color);
+		color: var(--muted-color);
+
+		font-size: var(--font-size-sm);
+	}
+
+	.date .time {
+		font-size: var(--font-size-lg);
+	}
+
+	main {
+		word-wrap: break-word;
+		overflow: auto;
+		display: flex;
+		flex-direction: column;
+		justify-content: top;
+
+		padding: var(--space-s);
+		padding-top: var(--space-l);
+		padding-bottom: var(--space-l);
+
+		gap: var(--space-xs);
+	}
+
+	main h2,
+	main p {
+		margin: 0;
+	}
+
+	.location {
+		font-size: var(--font-size-sm);
+	}
+
+	.offer-box {
+		display: flex;
+		justify-content: space-between;
+		/* align-items: center; */
+
+		background-color: var(--muted-color);
+		padding: var(--space-s);
+		border-radius: 16px;
+
+		font-size: var(--font-size-sm);
+		gap: var(--space-l);
+	}
+
+	.offer-box .description {
+		flex: 1;
+	}
+
+	.offer-box .description .description-paragraph {
+		padding-bottom: var(--space-xs);
+	}
+
+	.offer-box .button {
+		display: block;
+		background-color: var(--primary-color);
+		color: var(--muted-color);
+		border-radius: 16px;
+		padding: var(--space-2xs);
+		text-decoration: none;
+		font-size: var(--font-size-sm);
+	}
+
+	@media (max-width: 800px) {
+		article {
+			grid-template-columns: 1fr;
+		}
+
+		.date {
+			border-radius: 16px 16px 0 0;
+			padding: var(--space-m);
+			font-size: var(--font-size-m);
+		}
+
+		main {
+			padding-top: 0px;
+		}
+	}
+</style>
--- src/routes/events/[slug]/+page.ts
+++ src/routes/events/[slug]/+page.ts
@@ -1,12 +1,17 @@
-import { getEventsFromGoogleCalendar } from '../../../libs/events/get-events';
+import type { EventData } from '../../../libs/events/types';
 import type { PageLoad } from './$types';
 
-export const load: PageLoad = async ({ params }) => {
-	const events = await getEventsFromGoogleCalendar();
+export const load: PageLoad = async ({ params, fetch }) => {
+	const apiEvents = await fetch('/api/events');
+	const events = (await apiEvents.json()) as EventData[];
 
-	return {
-		event: events.find((event) => event.slug === params.slug)
-	};
+	return events
+		.map((event) => ({
+			...event,
+			startDate: new Date(event.startDate),
+			endDate: new Date(event.endDate)
+		}))
+		.find((event) => event.slug === params.slug);
 };
 
 export const prerender = true;
