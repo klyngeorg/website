7283668ba0df6fefc5aadfc7cd4d1156d82075f0
Simen A. W. Olsen <simen@padeia.com>
2024-09-22 16:14:56 +0200

    add another old url

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import sitemap from "@astrojs/sitemap";
 export default defineConfig({
   site: "https://klynge.org",
   redirects: {
+    "/about": "/medlem",
     "/about/articles": "/vedtekter",
     "/privacy": "/personvern",
     "/events/": "/aktiviteter",
